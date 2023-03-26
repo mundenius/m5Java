0cd929d30a6b8d90f6fed2c2dbc0b1f43734c78a
Mundenius <vsaldias97@gmail.com>
2023-03-26 15:16:10 -0300

    lista de capacitaciones, tweak de la pagina inicio, listo al parecer el 1

--- Ejercicio1/src/main/java/servlets/FormularioCrearCapacitacion.java
+++ Ejercicio1/src/main/java/servlets/FormularioCrearCapacitacion.java
@@ -3,6 +3,7 @@ package servlets;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,7 +30,10 @@ public class FormularioCrearCapacitacion extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		 // Obtener los valores de los campos del formulario
-        String nombreCap = request.getParameter("nombreCap");
+		ListaCapacitaciones lista1 = new ListaCapacitaciones();
+		Capacitacion capacitacion = new Capacitacion();
+		
+		String nombreCap = request.getParameter("nombreCap");
         String descripcion = request.getParameter("descripcion");
         String fechaCap = request.getParameter("fecha");
         String duracionCap = request.getParameter("duracion");
@@ -50,16 +54,80 @@ public class FormularioCrearCapacitacion extends HttpServlet {
         System.out.println(duracion);
 
         // Guardar los valores en los atributos de la solicitud
+        
+        
         request.setAttribute("nombre", nombreCap);
+        capacitacion.setNombreCap(nombreCap);
         request.setAttribute("descripcion", descripcion);
+        capacitacion.setDescripcion(descripcion);
         request.setAttribute("fecha", fecha);
+        capacitacion.setFechaCap(fechaCap);
         request.setAttribute("duracion", duracion);
+        capacitacion.setDuracionCap(duracionCap);
+        
+        lista1.agregarCapacitacion(capacitacion);
 
     	// Redirige a una pagina de confirmacion
-		 response.sendRedirect("Confirmacion.jsp");
+		 response.sendRedirect("confirmacionContacto.jsp");
+		 
+		 lista1.listarCapacitaciones(capacitacion);
 		 return;
 
 
 	}
-
+/** @publicClass se crea clase interna para manejar arraylist de capacitaciones */
+	public class Capacitacion {
+		String nombreCap;
+        String descripcion;
+        String fechaCap;
+        String duracionCap;
+        
+        public void Capacitacion() {
+        	
+        }
+        public void Capacitacion(String nombreCap, String descripcion, String fechaCap, String duracionCap) {
+        	this.nombreCap = nombreCap;
+        	this.descripcion = descripcion;
+        	this.fechaCap = fechaCap;
+        	this.duracionCap = duracionCap;
+        }
+        
+        /**@param nombre capacitacion */ 
+        public void setNombreCap(String nombreCap) {
+        	this.nombreCap = nombreCap;
+        }
+        public String getNombreCap() {
+        	return nombreCap;
+        }
+        
+        /**@param descripcion capacitacion */
+        public void setDescripcion(String descripcion) {
+        	this.descripcion = descripcion;
+        }
+        public String getDescripcion() {
+        	return descripcion;
+        }
+        
+        /**@param fecha capacitacion */
+        public void setFechaCap(String fechaCap) {
+        	this.fechaCap = fechaCap; 
+        }
+        public String getFechaCap() {
+        	return fechaCap;
+        }
+        
+        /**@param duracion capacitacion */
+        public void setDuracionCap(String duracionCap) {
+        	this.duracionCap = duracionCap;
+        }
+        public String getDuracionCap() {
+        	return duracionCap;
+        }
+        
+        @Override 
+        public String toString() {
+        	return "Nombre Capacitacion: " + getNombreCap() + "\nDescripcion Capacitacion: " + getDescripcion() 
+        	+ "\nFecha Capacitacion: " + getFechaCap() + "\nDuracion Capacitacion: " + getDuracionCap();
+        }
+	}
 }
