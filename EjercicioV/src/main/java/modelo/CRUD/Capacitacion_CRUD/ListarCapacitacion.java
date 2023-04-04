a5f058380a099fde743bd8f0e1fd0a9afd97a086
mundenius <121872746+mundenius@users.noreply.github.com>
2023-04-04 15:33:57 -0400

    V: modelado de carpetas listo, falta desarrollar clases y trabajar en las cosas del DAO

---
+++ EjercicioV/src/main/java/modelo/CRUD/Capacitacion_CRUD/ListarCapacitacion.java
@@ -0,0 +1,60 @@
+package modelo.CRUD.Capacitacion_CRUD;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.DAO.CapacitacionDAO.ImplCapacitacionDAO;
+import modelo.DAO.CapacitacionDAO.ICapacitacionDAO;
+
+/**
+ * Servlet implementation class ListarCapacitacion
+ */
+//@WebServlet("/ListarCapacitacion")
+public class ListarCapacitacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListarCapacitacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		Connection conn = Singleton.getConnection();
+		try {
+			System.out.println("entering try/catch sv listar capacitaciones...");
+			ICapacitacionDAO capdao = new ImplCapacitacionDAO();
+			System.out.println("se creo la listapersona e instancia de perdao");
+//			listapersona = perdao.listarTodos();	otra forma
+			request.setAttribute("listacapacitacion", capdao.listarTodos()); //listapersona	otra forma
+			System.out.println("request.setAttribute de ListarCapacitacion exitoso"); //DEBUG
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/listaCapacitaciones.jsp");
+			dispatcher.forward(request, response);
+			
+			System.out.println("se despacho el forward de request,response");
+//			conn.close();
+		}catch(Exception e) {
+			System.out.println(e + " LISTAR CAPACITACION SERVLET");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ EjercicioV/src/main/java/modelo/DAO/UsuarioDAO/ImplUsuarioDAO.java
@@ -0,0 +1,33 @@
+package modelo.DAO.UsuarioDAO;
+
+import java.util.List;
+
+import modelo.clases.Usuario;
+
+public class ImplUsuarioDAO implements IUsuarioDAO{
+
+	@Override
+	public List<Usuario> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void registrar(Usuario d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizar(Usuario d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminar(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
